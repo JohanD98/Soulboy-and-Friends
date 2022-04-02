2493fb82b84a743a22220217fa0c3d7e18f7aa52
Cube <samuel.karlsson_03@hotmail.com>
2022-04-02 20:24:34 +0200

    Enemy wandering added

--- Assets/Core/Scripts/Enemy.cs
+++ Assets/Core/Scripts/Enemy.cs
@@ -1,20 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : Entity
 {
-    
+    [SerializeField] NavMeshAgent m_agent;
+    [SerializeField] Vector3 m_wanderPos;
+    [SerializeField] float m_wanderRadius;
+    [SerializeField] float m_minWanderCooldown;
+    [SerializeField] float m_maxWanderCooldown;
+    private float m_lastWanderTime;
+    private bool m_isStanding;
+    private float m_nextWanderTime;
 
     // Start is called before the first frame update
     void Start()
     {
         EntityStart();
+        m_nextWanderTime = Random.Range(m_minWanderCooldown, m_maxWanderCooldown);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckWander();
+    }
+
+    private void CheckWander()
+    {
+        if(m_isStanding)
+        {
+            m_lastWanderTime += Time.deltaTime;
+            if(m_lastWanderTime >= m_nextWanderTime)
+            {
+                Wander();
+            }
+        }
+        else if(!m_agent.hasPath)
+        {
+            m_isStanding = true;
+        }
+
+    }
+
+    private void Wander()
+    {
+        m_lastWanderTime = 0;
+        m_nextWanderTime = Random.Range(m_minWanderCooldown, m_maxWanderCooldown);
+        m_agent.SetDestination(Random.insideUnitSphere*m_wanderRadius + m_wanderPos);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(m_wanderPos, m_wanderRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(m_wanderPos, 0.25f);
     }
 }
